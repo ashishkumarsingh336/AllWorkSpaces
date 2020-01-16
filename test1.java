510fcf31e9272dc98400a97f4d4b70c3d89e6602
ashish <ashishkumarsingh336@gmail.com>
2020-01-16 17:20:09 +0530

    modification done in test1 (childMaster)

--- test1.java
+++ test1.java
@@ -7,8 +7,10 @@ private static int j=20;
 public void m1(){
 
 System.out.println(Test.i);
-   //modification done
+   //modification done 
   System.out.println("Print  m1 ");
+
+  System.out.println("Print m1 again");
 
 }
 
