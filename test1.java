c1522b71d323a4fd56f2c7e560d6e09efd4a05a2
ashish <ashishkumarsingh336@gmail.com>
2020-01-16 17:06:32 +0530

    add test1.java

---
+++ test1.java
@@ -0,0 +1,16 @@
+public class Test{
+
+private static int i=10;
+
+private static int j=20;
+
+public void m1(){
+
+System.out.println(Test.i);
+   //modification done
+  System.out.println("Print  m1 ");
+
+}
+
+
+}
